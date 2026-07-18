cd874a3f78ce4b8497bb42e916b0f231ed62de53
sancsoffline25 <ytblueanimation@gmail.com>
2026-07-18 15:17:46 -0300

    hola

---
+++ src/main/java/com/mycompany/worms2therevenge/Worms2TheRevenge.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.worms2therevenge;
+
+/**
+ *
+ * @author Santiago Guinel y Lautaro Gutierrez
+ */
+public class Worms2TheRevenge {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
